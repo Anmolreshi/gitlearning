867e057dbd12a750394e09ecee0d4b79afc293ea
Anmolreshi <anmolreshi@gmail.com>
2020-05-16 23:03:50 +0530

    Dev branch change

---
+++ code.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("welcome to coding");
+	return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include< stdio.h>
+int int main(int argc, char const *argv[])
+{
+printf("hello world\n");
+printf("anmol reshi project\n");
+	return 0;
+}
